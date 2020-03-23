37a2869f1aa4b0fb10979d5f387f2b1263b303b8
Denis Bolkovskis <denisb42@gmail.com>
2020-03-23 07:31:48 +0200

    read csv

--- quiz/main.go
+++ quiz/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	csvfile, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(csvfile)
+
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Question: %s Answer %s\n", record[0], record[1])
+	}
+
+	csvfile.Close()
 }

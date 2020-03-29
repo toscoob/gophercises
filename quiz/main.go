5c1123e6162bf6e1e6ac968fbfc47f328d6c8aa0
Denis Bolkovskis <denisb42@gmail.com>
2020-03-29 17:55:59 +0300

    basic quiz

--- quiz/main.go
+++ quiz/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,6 +19,11 @@ func main() {
 	// Parse the file
 	r := csv.NewReader(csvfile)
 
+	questions := 0
+	answers := 0
+
+	reader := bufio.NewReader(os.Stdin)
+
 	// Iterate through the records
 	for {
 		// Read each record from csv
@@ -27,8 +34,20 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Question: %s Answer %s\n", record[0], record[1])
+		questions++
+		//fmt.Printf("Question: %s Answer %s\n", record[0], record[1])
+		fmt.Printf("Question #%d: %s = ", questions, record[0])
+		text, _ := reader.ReadString('\n')
+		//fmt.Println(text)
+		if strings.TrimRight(text, "\n") == record[1] {
+			fmt.Println("Correct")
+			answers++
+		} else {
+			fmt.Printf("Wrong. Correct answer: %s\n", record[1])
+		}
 	}
 
 	csvfile.Close()
+
+	fmt.Printf("Correct answers: %d/%d\n", answers, questions)
 }

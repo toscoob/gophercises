54875744013bab56f5693942e9b80fb3ae1485ce
Denis Bolkovskis <denisb42@gmail.com>
2020-04-14 12:00:21 +0300

    story basics and json read

--- cyoa/main/main.go
+++ cyoa/main/main.go
@@ -1,5 +1,32 @@
 package main
 
-// load file
-// parse json to story
-// replay contents
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"github.com/gophercises/cyoa"
+)
+
+// go run main/main.go
+
+// write console player which shows story entry and waits for input
+
+func main(){
+	jsonFilename := flag.String("j", "scenario.json", "json file with scenario")
+
+	flag.Parse()
+
+	jsonContent, err := ioutil.ReadFile(*jsonFilename)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyoa.ReadStoryJSON(jsonContent)
+	if err != nil {
+		panic(err)
+	}
+
+	for k, v := range story {
+		fmt.Printf("arc: %s\ntitle: %s\n", k, v.Title)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/gophercises
 
 go 1.14
+
+require (
+	github.com/boltdb/bolt v1.3.1
+	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4 // indirect
+	gopkg.in/yaml.v2 v2.2.8
+)
